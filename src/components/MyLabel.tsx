c163c0ab7dcb20a3876261f2cc95f0446da58583
Franklin Martinez <fml010603@gmail.com>
2021-11-16 12:21:18 -0600

    Primer commit

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,43 @@
+
+import './my-label.css'
+
+export interface PropsLabel {
+    /**
+     *  Mensaje a mostrar en la etiqueta
+     */
+    label: string;
+    /**
+     *  Tamaños a mostrar
+     */
+    size: 'normal' | 'h1' | 'h2' | 'h3';
+    /**
+     *  Tipo de color a mostrar
+     */
+    color: 'primary' | 'secondary' | 'tertiary';
+    /**
+     *  Capitalizacion de las letras
+     */
+    allCaps: boolean;
+    /**
+     *  Color de la fuente
+     */
+    fontColor?: string;
+}
+
+export const MyLabel = ({ 
+    label = 'No Label', 
+    size = 'normal',
+    color = 'primary',
+    allCaps = false,
+    fontColor
+}: PropsLabel) => {
+
+    return (
+        <span 
+            className={`label ${size} ${color}`}
+            style={{color: fontColor}}
+        >
+            { (!allCaps) ?  label : label.toUpperCase() }
+        </span>
+    )
+}
---
+++ src/stories/components/MyLabel.stories.tsx
@@ -0,0 +1,49 @@
+import { MyLabel } from '../../components/MyLabel';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+
+export default {
+    title: 'UI/Label',
+    component: MyLabel,
+    argTypes: {
+        size: { control: 'select' },
+        color: { control: 'select' },
+        fontColor: { control: 'color' }
+
+    }
+
+} as ComponentMeta<typeof MyLabel>
+
+const Template: ComponentStory<typeof MyLabel> = (args) => <MyLabel {...args} />
+
+export const Basic = Template.bind({});
+Basic.args = {
+    label: 'Hello World',
+    size: 'normal',
+    allCaps: false,
+};
+
+export const AllCaps = Template.bind({});
+AllCaps.args = {
+    allCaps: true,
+    size: 'normal'
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+    size: 'normal',
+    color:'secondary'
+};
+
+export const Tertiary = Template.bind({});
+Tertiary.args = {
+    size: 'normal',
+    color:'secondary'
+};
+
+export const CustomFontColor = Template.bind({});
+CustomFontColor.args = {
+    size: 'h1',
+    color:'secondary',
+    fontColor: '#5517ac'
+};
